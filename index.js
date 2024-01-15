3ffdf16696011652bab8a3159df1036940e3a5c0
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-01-15 18:20:13 +0100

    challenge solved

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/whoami", function (req, res) {
+  
+  var ipAddress = req.headers["x-forwarded-for"];
+  var lang = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+
+  res.json({ ipaddress: ipAddress, language: lang, software: software });
 });
 
 // listen for requests :)
